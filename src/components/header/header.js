4906e42194c2e89c805c88dac23e27af64482119
Tirsog <tirsog@gmail.com>
2022-11-25 11:00:44 +0000

    user li

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -21,7 +21,7 @@ function Header(props) {
         <li><a href="#profile">Profile</a></li>
         <li><a href="#posts"> Posts </a></li>
         <li><a href="#contact"> Contact </a></li>
-        <li className="welcome"><a href="#welcome"></a>        </li>
+        <li className="welcome">Marwa D.<a href="#welcome"></a>        </li>
       </ul>
     </nav>
   );
