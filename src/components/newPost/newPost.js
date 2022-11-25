27a53c9a2f0ba488d9438eb730030d574ac28875
Tirsog <tirsog@gmail.com>
2022-11-25 10:25:09 +0000

    Changes

--- src/components/newPost/newPost.js
+++ src/components/newPost/newPost.js
@@ -1,6 +1,6 @@
 import "./newPost.css";
 import Button from "../button/button.js"
-import logo from "../../fonts_images/logo1.png"
+import newPost from "../../fonts_images/newPost.png";
 import {useState} from 'react'
 
 
@@ -9,61 +9,50 @@ export default function NewPost(props) {
     const [title,setTitle]=useState("")
     const [des,setDes]=useState("")
     const [stack,setStack]=useState("")
-    const [payload,setPayload]= useState({
-        user_name : null,
-        title : null ,
-        description : null, 
-        stack: null
+    // const [payload,setPayload]= useState({
+    //     user_name : null,
+    //     title : null ,
+    //     description : null, 
+    //     stack: null
     
-    })
+    // })
 
     function onChangeName(e){
         let text = e.target.value
         setName(text)
-     
 
-        console.log(name)
     }
     function onChangeTitle(e){
         let text = e.target.value
-        
         setTitle(text)
-        console.log(title)
     }
     function onChangeDes(e){
-        let text = e.target.value
-        
+        let text = e.target.value 
         setDes(text)
-        console.log(des)
+
     }
     function onChangeStack(e){
         let text = e.target.value
-        
         setStack(text)
-        console.log(stack)
+  
     }
 
     async function onClick(e){
-        
         e.preventDefault()
-        // let key = {
-        //     user_name : null,
-        //     title : null ,
-        //     description : null, 
-        //     stack: null
-        
-        // }
-        let obj = {...payload,user_name:name,title:title,description:des,stack:stack}
-        setPayload(obj)
+        let obj = {user_name:name,title:title,description:des,stack:stack}
+        // setPayload(obj)
         
         console.log('button clicked')
-        await fetch("https://duckmate-back-production.up.railway.app/api/post",{method:'POST',headers:{'accept': 'application/json','content-type':'application/json'},
-        body: JSON.stringify(payload)
+        await fetch("http://localhost:3001/api/post",{method:'POST',headers:{'accept': 'application/json','content-type':'application/json'},
+        body: JSON.stringify(obj)
         })
         .then(response => response.json())
         .then(response => console.log(JSON.stringify(response)))
-         e.target.reset() 
+       
          
+    }
+    function clearInput(){
+
     }
     
   return (
@@ -72,9 +61,9 @@ export default function NewPost(props) {
 
 
             <div className="newPost-container">
-                <img className='newPostImage' src={logo} alt='logo'/>
+                <img className='newPostImage' src={newPost} alt='logo'/>
                   
-                <form >
+                <form onSubmit={clearInput} >
                     <label> Username: </label>
                     <input type="text" userName="userName" placeholder="Username" maxlength="20" required onChange={onChangeName}/>
 
